f5448014057802be86f8ecbd8bb7e026f938b452
Hector <baseballhector@gmail.com>
2024-06-16 17:18:13 -0400

    Initial home page

--- src/components/PageComponents/Home/Home.jsx
+++ src/components/PageComponents/Home/Home.jsx
@@ -1,7 +1,23 @@
+import Navbar from "../../UI/Navbar/Navbar";
+
 import styles from "./Home.module.scss";
 
 const Home = () => {
-  return <div className={styles.home}>Home page</div>;
+  return (
+    <>
+      <Navbar />
+      <div className={styles.home}>
+        <h1>Links</h1>
+        <a href="">Sign Up</a>
+        <br />
+        <a href="/login">Log in</a>
+        <br />
+        <a href="/search">All Authors</a>
+        <br />
+        <a href="/test">Test page for fetching data</a>
+      </div>
+    </>
+  );
 };
 
 export default Home;
