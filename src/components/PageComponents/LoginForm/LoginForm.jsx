6ed373384c85d19b1f425498be5fbe38ae725abf
Hector <baseballhector@gmail.com>
2024-06-16 20:35:39 -0400

    Initial log in form

---
+++ src/components/PageComponents/LoginForm/LoginForm.jsx
@@ -0,0 +1,38 @@
+import Navbar from "../../UI/Navbar/Navbar";
+import styles from "./LoginForm.module.scss";
+
+import loginUtil from "../../../helpers/loginUtil";
+
+const LoginForm = () => {
+  return (
+    <div className={styles["page-container"]}>
+      <Navbar />
+
+      <div className={styles["form-container"]}>
+        <form className={styles.form} onSubmit={loginUtil}>
+          <div className="form-title">
+            <h1>Log In</h1>
+          </div>
+
+          <div className={styles["form-group-container"]}>
+            <div className={styles["form-group"]}>
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" />
+            </div>
+
+            <div className={styles["form-group"]}>
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" />
+            </div>
+          </div>
+
+          <div className="form-submit">
+            <button type="submit">Log In</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
