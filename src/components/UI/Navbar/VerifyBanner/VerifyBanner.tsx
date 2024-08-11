b49e499f823de2d605f35da29fdf69c25557ecac
Hector <baseballhector@gmail.com>
2024-08-11 17:05:32 -0400

    Banner component for unverified users

---
+++ src/components/UI/Navbar/VerifyBanner/VerifyBanner.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+// Interfaces
+import { Iuser } from "../Navbar";
+
+interface IProps {
+  user: Iuser | false | null;
+}
+
+const VerifyBanner = ({ user }: IProps) => {
+  const [showBanner, setshowBanner] = useState(true);
+  if (!user) {
+    return null;
+  }
+
+  if (user === null) {
+    return;
+  }
+
+  if (user.verified) {
+    return null;
+  }
+
+  function calculateBannerClasses() {
+    var bannerClasses =
+      "relative flex flex-row justify-center items-center w-screen h-auto bg-yellow-100";
+
+    if (!showBanner) {
+      bannerClasses += " ";
+      bannerClasses += "hidden";
+    }
+
+    return bannerClasses;
+  }
+
+  function closeBannerHandler() {
+    setshowBanner(false);
+  }
+
+  return (
+    <div className={calculateBannerClasses()}>
+      <div
+        onClick={closeBannerHandler}
+        className="absolute left-0 text-2xl font-bold px-2 cursor-pointer hover:text-slate-500"
+      >
+        X
+      </div>
+      <div className="text-3xl w-auto">
+        You have not verified your account yet. Click here to verify.
+      </div>
+      <a
+        className="text-sky-400 font-bold text-lg p-3 border-2 border-sky-400 rounded-md cursor-pointer
+      
+    hover:text-white 
+    hover:bg-sky-400"
+      >
+        Verify
+      </a>
+    </div>
+  );
+};
+
+export default VerifyBanner;
