aa27f8ecb88baff81014f2bdc6c40fb293ece6c3
Hector <baseballhector@gmail.com>
2024-08-13 18:20:50 -0400

    Added animations for floating leaning to left and right

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,22 @@ export default {
       fontFamily: {
         FuzzyBubbles: ["Fuzzy-Bubbles"],
       },
+      keyframes: {
+        floatLeft: {
+          "0%": { transform: "translateY(0px) rotate(-6deg)" },
+          "50%": { transform: "translateY(10px) rotate(-6deg)" },
+          "100%": { transform: "translateY(0px) rotate(-6deg)" },
+        },
+        floatRight: {
+          "0%": { transform: "translateY(0px) rotate(6deg)" },
+          "50%": { transform: "translateY(10px) rotate(6deg)" },
+          "100%": { transform: "translateY(0px) rotate(6deg)" },
+        },
+      },
+      animation: {
+        "float-left": "floatLeft 3s ease-in-out infinite",
+        "float-right": "floatRight 3s ease-in-out infinite",
+      },
     },
   },
   plugins: [],
