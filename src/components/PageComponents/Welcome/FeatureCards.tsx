da6f417ae54a003701c9678f757718167a34d931
Hector <baseballhector@gmail.com>
2024-08-19 17:32:19 -0400

    Width and margin changes to remove whitespace on right of screen

--- src/components/PageComponents/Welcome/FeatureCards.tsx
+++ src/components/PageComponents/Welcome/FeatureCards.tsx
@@ -4,25 +4,30 @@ type FeaturesDataTypes = {
 
 const FeatureCards = ({ featuresData }: FeaturesDataTypes) => {
   const featureCards = featuresData.map((cardData, index) => {
+    var containerClassNameText = "";
     var classNameText = "";
 
     if (index % 2 === 0) {
       classNameText =
-        "animate-float-left flex text-center justify-center items-center border-2 border-black size-48 mr-36 desktop:size-96 desktop:mr-[48rem]";
+        "animate-float-left flex text-center justify-center items-center border-2 border-black size-48 ml-8 desktop:size-96 desktop:ml-48";
+
+      containerClassNameText = "relative flex h-full w-screen justify-start";
     } else {
       classNameText =
-        "animate-float-right flex text-center justify-center items-center border-2 border-black size-48 ml-36 desktop:size-96 desktop:ml-[48rem]";
+        "animate-float-right flex text-center justify-center items-center border-2 border-black size-48 mr-8 desktop:size-96 desktop:mr-48";
+
+      containerClassNameText = "relative flex h-full w-screen justify-end";
     }
 
     return (
-      <div className="relative flex w-full h-full justify-center">
+      <div key={index} className={containerClassNameText}>
         <div className={classNameText}>{cardData}</div>
       </div>
     );
   });
 
   return (
-    <div className="grid grid-flow-row my-12 mx-4 gap-y-6 font-FuzzyBubbles">
+    <div className="grid grid-flow-row my-12 gap-y-6 w-screen font-FuzzyBubbles">
       {featureCards}
     </div>
   );
