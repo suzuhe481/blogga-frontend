2d364032564a3ad781d30a799e4139f7bcf76ea7
Hector <baseballhector@gmail.com>
2024-08-19 18:20:38 -0400

    Added shadows to cards

--- src/components/PageComponents/Welcome/FeatureCards.tsx
+++ src/components/PageComponents/Welcome/FeatureCards.tsx
@@ -9,12 +9,12 @@ const FeatureCards = ({ featuresData }: FeaturesDataTypes) => {
 
     if (index % 2 === 0) {
       cardClasses =
-        "animate-float-left flex text-center justify-center items-center border-2 border-black size-48 ml-8 desktop:size-96 desktop:ml-48";
+        "animate-float-left flex text-center justify-center items-center border-2 border-black size-48 ml-8 desktop:size-96 desktop:ml-48 shadow-sky-400 shadow-md";
 
       containerClasses = "relative flex h-full w-screen justify-start";
     } else {
       cardClasses =
-        "animate-float-right flex text-center justify-center items-center border-2 border-black size-48 mr-8 desktop:size-96 desktop:mr-48";
+        "animate-float-right flex text-center justify-center items-center border-2 border-black size-48 mr-8 desktop:size-96 desktop:mr-48 shadow-sky-400 shadow-md";
 
       containerClasses = "relative flex h-full w-screen justify-end";
     }
