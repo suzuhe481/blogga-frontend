141ac0adfc3f74bb93a3ae10660048525e0a1579
Hector <baseballhector@gmail.com>
2024-08-20 17:05:35 -0400

    Importing and using the Card component

--- src/components/PageComponents/Welcome/FeatureCards.tsx
+++ src/components/PageComponents/Welcome/FeatureCards.tsx
@@ -1,29 +1,12 @@
+import Card from "./Card";
+
 type FeaturesDataTypes = {
   featuresData: string[];
 };
 
 const FeatureCards = ({ featuresData }: FeaturesDataTypes) => {
   const featureCards = featuresData.map((cardData, index) => {
-    var containerClasses = "";
-    var cardClasses = "";
-
-    if (index % 2 === 0) {
-      cardClasses =
-        "animate-float-left flex text-center justify-center items-center border-2 border-black size-48 ml-8 desktop:size-96 desktop:ml-48 shadow-sky-400 shadow-md";
-
-      containerClasses = "relative flex h-full w-screen justify-start";
-    } else {
-      cardClasses =
-        "animate-float-right flex text-center justify-center items-center border-2 border-black size-48 mr-8 desktop:size-96 desktop:mr-48 shadow-sky-400 shadow-md";
-
-      containerClasses = "relative flex h-full w-screen justify-end";
-    }
-
-    return (
-      <div key={index} className={containerClasses}>
-        <div className={cardClasses}>{cardData}</div>
-      </div>
-    );
+    return <Card cardData={cardData} key={index} index={index} />;
   });
 
   return (
