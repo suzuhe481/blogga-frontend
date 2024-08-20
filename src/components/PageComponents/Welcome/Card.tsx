9de9c949f663159e96c9a42fbb315a6721c53968
Hector <baseballhector@gmail.com>
2024-08-20 17:04:58 -0400

    Separated each feature card into a separate component

---
+++ src/components/PageComponents/Welcome/Card.tsx
@@ -0,0 +1,27 @@
+type CardTypes = {
+  cardData: string;
+  index: number;
+};
+
+const Card = ({ cardData, index }: CardTypes) => {
+  var containerClasses = "relative flex h-full w-screen ";
+  var cardClasses =
+    "flex text-center hover:[animation-play-state:paused] justify-center items-center border-2 border-black size-48 shadow-sky-400 shadow-md desktop:size-96 ";
+
+  if (index % 2 === 0) {
+    cardClasses += "animate-float-left ml-8 desktop:ml-48 ";
+
+    containerClasses += "justify-start";
+  } else {
+    cardClasses += "animate-float-right mr-8 desktop:mr-48";
+
+    containerClasses += "justify-end";
+  }
+
+  return (
+    <div key={index} className={containerClasses}>
+      <div className={cardClasses}>{cardData}</div>
+    </div>
+  );
+};
+export default Card;
