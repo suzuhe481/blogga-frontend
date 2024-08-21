a52da5c588e552ea7941ad3d979a08bde839a9af
Hector <baseballhector@gmail.com>
2024-08-21 18:38:50 -0400

    Height, margin changes

--- src/components/UI/Navbar/Navbar.tsx
+++ src/components/UI/Navbar/Navbar.tsx
@@ -37,8 +37,8 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className="navbar-container flex flex-col justify-center items-center h-auto min-h-28 py-2 w-full">
-      <div className="navbar flex flex-row justify-between items-center h-20 w-full mx-2">
+    <div className="navbar-container flex flex-col justify-center items-center m-2 mb-0 desktop:mx-48">
+      <div className="navbar flex flex-row justify-between items-center h-12 desktop:h-20 w-full mx-2">
         <LogoContainer />
         {!user || null ? <MenuPublic /> : <ProfileContainer user={user} />}
       </div>
