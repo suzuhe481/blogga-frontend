36fc1e493e98d975d3269405981c77451830e878
Hector <baseballhector@gmail.com>
2024-08-21 18:38:14 -0400

    Gap, text size changes

--- src/components/UI/Navbar/MenuPublic/MenuPublic.tsx
+++ src/components/UI/Navbar/MenuPublic/MenuPublic.tsx
@@ -1,15 +1,15 @@
 const MenuPublic = () => {
   return (
-    <div className="flex flex-row text-nowrap justify-center items-center gap-6 h-fullk">
+    <div className="flex flex-row text-nowrap justify-center items-center gap-1 desktop:gap-6 h-fullk">
       <a
         href="/login"
-        className="font-bold hover:text-sky-400 p-2 flex flex-row justify-center items-center"
+        className="font-bold hover:text-sky-400 text-sm desktop:text-lg text-nowrap p-2 flex flex-row justify-center items-center"
       >
         Log in
       </a>
       <a
         href="/signup"
-        className="text-sky-400 font-bold text-lg p-3 border-2 border-sky-400 rounded-md cursor-pointer
+        className="text-sky-400 font-bold text-sm text-nowrap desktop:text-lg p-2 border-2 border-sky-400 rounded-md cursor-pointer
     
     hover:text-white 
     hover:bg-sky-400"
