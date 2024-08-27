67506fc4693b2ff9dbe079dcdf176b0ab8c44313
Hector <baseballhector@gmail.com>
2024-08-27 18:28:47 -0400

    Removed unneeded states, useEffects, components

--- src/components/UI/Navbar/Navbar.tsx
+++ src/components/UI/Navbar/Navbar.tsx
@@ -1,48 +1,11 @@
-import { useState, useEffect } from "react";
-
-import VerifyBanner from "./VerifyBanner/VerifyBanner";
 import LogoContainer from "./LogoContainer/LogoContainer";
-import MenuPublic from "./MenuPublic/MenuPublic";
-import ProfileContainer from "./ProfileContainer/ProfileContainer";
-
-import navbarDataUtil from "../../../helpers/navbarDataUtil";
-
-export interface Iuser {
-  first_name: string;
-  last_name: string;
-  verified: Boolean;
-}
 
 const Navbar = () => {
-  // false = before fetching user
-  // null = no user
-  const [user, setUser] = useState<Iuser | false | null>(false);
-
-  // Retrieves and sets user data for navbar.
-  useEffect(() => {
-    navbarDataUtil().then((result) => {
-      if (result.error === true) {
-        setUser(null);
-        return;
-      }
-
-      const user: Iuser = {
-        first_name: result.first_name,
-        last_name: result.last_name,
-        verified: result.verified,
-      };
-
-      setUser(user);
-    });
-  }, []);
-
   return (
     <div className="navbar-container flex flex-col justify-center items-center m-2 mb-0 desktop:mx-48">
       <div className="navbar flex flex-row justify-between items-center h-12 desktop:h-20 w-full mx-2">
         <LogoContainer />
-        {!user || null ? <MenuPublic /> : <ProfileContainer user={user} />}
       </div>
-      <VerifyBanner user={user} />
     </div>
   );
 };
