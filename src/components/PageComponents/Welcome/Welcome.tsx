4f6a8c81efd3fb79623dfd889686ed770356b1db
Hector <baseballhector@gmail.com>
2024-08-27 18:30:02 -0400

    Importing and using the Header component

--- src/components/PageComponents/Welcome/Welcome.tsx
+++ src/components/PageComponents/Welcome/Welcome.tsx
@@ -1,6 +1,7 @@
 import Navbar from "../../UI/Navbar/Navbar";
 import LOGO_WHITE from "../../../assets/images/LOGO_WHITE.png";
 
+import Header from "./Header";
 import FeatureCards from "./FeatureCards";
 import Footer from "./Footer";
 
@@ -15,18 +16,7 @@ const Welcome = () => {
   return (
     <div>
       <Navbar />
-
-      <div className="flex flex-col shadow-xl justify-center gap-2 items-center h-48 bg-sky-400 text-2xl desktop:text-5xl text-white font-FuzzyBubbles font-bold desktop:h-96 desktop:gap-0">
-        Welcome to
-        <img
-          src={LOGO_WHITE}
-          className="h-20 object-contain aspect-auto desktop:h-60"
-        />
-        <a className="border-2 border-sky-200 rounded-full px-2 py-1 text-lg desktop:text-4xl desktop:4xl desktop:px-4 desktop:py-2 hover:bg-sky-600 text-white">
-          <button>Browse</button>
-        </a>
-      </div>
-
+      <Header />
       <FeatureCards featuresData={FeaturesData} />
       <Footer />
     </div>
