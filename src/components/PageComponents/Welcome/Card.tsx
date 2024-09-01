84c5907584347d2f13a81d294a43ebb9ac4e8d23
Hector <baseballhector@gmail.com>
2024-09-01 19:24:53 -0400

    Offset feature cards on mobile

--- src/components/PageComponents/Welcome/Card.tsx
+++ src/components/PageComponents/Welcome/Card.tsx
@@ -4,14 +4,18 @@ type CardTypes = {
 };
 
 const Card = ({ cardData, index }: CardTypes) => {
-  var containerClasses = "relative flex h-full justify-center ";
+  var containerClasses = "relative flex h-full desktop:justify-center ";
   var cardClasses =
     "flex text-center hover:[animation-play-state:paused] m-2 justify-center items-center border-2 border-black size-48 shadow-sky-400 shadow-md desktop:size-96 ";
 
   if (index % 2 === 0) {
-    cardClasses += "animate-float-left ";
+    cardClasses += "animate-float-left ml-8 ";
+
+    containerClasses += "justify-start";
   } else {
-    cardClasses += "animate-float-right ";
+    cardClasses += "animate-float-right mr-8 ";
+
+    containerClasses += "justify-end";
   }
 
   return (
