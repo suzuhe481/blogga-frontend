e3c720296d70f1329bb041c07eeec2391cb59974
Hector <baseballhector@gmail.com>
2024-09-02 18:51:50 -0400

    New settings option in dropdown

--- src/components/UI/Navbar/ProfileContainer/ProfileContainer.tsx
+++ src/components/UI/Navbar/ProfileContainer/ProfileContainer.tsx
@@ -99,6 +99,15 @@ const ProfileContainer = ({ user }: IProps) => {
       {/* Separator */}
       <hr />
 
+      <div className="flex flex-col justify-center items-center relative w-36 desktop:w-44 h-14 p-1 hover:bg-sky-600 px-2">
+        <button className="flex flex-col justify-center items-start w-full h-full">
+          Settings
+        </button>
+      </div>
+
+      {/* Separator */}
+      <hr />
+
       <div className="flex flex-col justify-center items-center relative w-36 desktop:w-44 h-14 p-1 rounded-b-lg hover:bg-sky-600 px-2">
         <button
           className="flex flex-col justify-center items-start w-full h-full"
