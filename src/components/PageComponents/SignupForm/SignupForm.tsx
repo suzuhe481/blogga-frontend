e4831194e8de3008dc670a62a552e7dc297b210f
Hector <baseballhector@gmail.com>
2024-09-09 19:05:37 -0400

    Using form page components. Used inline styling for sliding form pages with margins

--- src/components/PageComponents/SignupForm/SignupForm.tsx
+++ src/components/PageComponents/SignupForm/SignupForm.tsx
@@ -1,63 +1,35 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 import Navbar from "../../UI/Navbar/Navbar";
 import Footer from "../Welcome/Footer";
-// import styles from "./SignupForm.module.scss";
+
+import FormPageOne from "./FormPageOne";
+import FormPageTwo from "./FormPageTwo";
+import FormPageThree from "./FormPageThree";
 
 import LOGO_BLUE from "../../../assets/images/LOGO_BLUE.png";
 
 import signUpUtil from "../../../helpers/signupUtil";
 
 const SignupForm = () => {
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
   const [formPage, setFormPage] = useState(0);
 
-  const confirmPasswordInputRef = useRef<HTMLInputElement>(null);
-
+  // Used in inline css due to difficulty of implementing dynamically in Tailwind.
   // Sets the left margin based on the current form page to animate slide appropriately.
-  var slidingFormClasses =
-    "flex justify-center items-center w-full transition-all duration-300 ";
-  slidingFormClasses += `ml-[-${formPage * 100}%]`;
-  // slidingFormClasses += `ml-[-100%]`;
+  // -100 * (number of forms - 1) / totalforms
+  const defaultMarginValue = (-100 * 2) / 3; // -66.6% or 2/3 of -100
+  const inlineMarginLeft = { marginLeft: `${formPage * defaultMarginValue}%` };
 
-  const passwordOnChangeHandler = (
-    e: React.KeyboardEvent<HTMLInputElement>
-  ) => {
-    const event = e.target as HTMLInputElement;
-    setPassword(event.value);
-  };
-
-  const confirmPasswordOnChangeHandler = (e: React.KeyboardEvent) => {
-    const event = e.target as HTMLInputElement;
-
-    setConfirmPassword(event.value);
-  };
-
-  const isPasswordsEqual = () => {
-    if (confirmPasswordInputRef.current === null) {
-      return;
-    }
-
-    // Changes password input's border color to black/red if there is error.
-    if (password === confirmPassword) {
-      confirmPasswordInputRef.current.style.borderColor = "#ccc";
-
-      return true;
-    } else {
-      confirmPasswordInputRef.current.style.borderColor = "red";
-
-      return false;
-    }
-  };
-
-  const handleSubmit = (e: any) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
 
-    if (isPasswordsEqual()) {
-      signUpUtil(e);
+    const result = await signUpUtil(e);
+    // If signup successful,
+    if (result === "good") {
+      // Go to final success page
+      nextPage();
     } else {
-      console.log("form has errors");
+      console.log("error");
     }
   };
 
@@ -87,92 +59,22 @@ const SignupForm = () => {
           <div className="w-full overflow-hidden">
             <form
               onSubmit={handleSubmit}
-              className="flex flex-col justify-center items-center gap-12 w-[200%] "
+              className="flex flex-col justify-center items-center gap-12 w-[300%] "
             >
-              <div className={slidingFormClasses}>
-                <div className="flex flex-col w-1/2 gap-4">
-                  <h1 className="text-4xl font-bold">Login Info</h1>
-                  <div className="flex flex-col">
-                    <label htmlFor="email">Email</label>
-                    <input
-                      type="text"
-                      name="email"
-                      required
-                      className="shadow-black shadow-sm border-transparent border-4 p-1 focus:border-black outline-none rounded-lg"
-                    />
-                  </div>
-                  <div className="flex flex-col">
-                    <label htmlFor="password">Password</label>
-                    <input
-                      type="password"
-                      name="password"
-                      required
-                      onKeyUp={passwordOnChangeHandler}
-                      className="shadow-black shadow-sm border-transparent border-4 p-1 focus:border-black outline-none rounded-lg"
-                    />
-                  </div>
-                  <div className="flex flex-col">
-                    <label htmlFor="confirm_password">Confirm password</label>
-                    <input
-                      type="password"
-                      name="confirm_password"
-                      required
-                      onKeyUp={confirmPasswordOnChangeHandler}
-                      ref={confirmPasswordInputRef}
-                      className="shadow-black shadow-sm border-transparent border-4 p-1 focus:border-black outline-none rounded-lg"
-                    />
-                  </div>
-                  <div className="flex flex-row justify-center items-center h-16 w-full">
-                    <button
-                      type="button"
-                      onClick={nextPage}
-                      className="h-full w-full bg-sky-400"
-                    >
-                      Next
-                    </button>
-                  </div>
+              <div
+                className="flex justify-center items-center w-full transition-all duration-300"
+                style={inlineMarginLeft}
+              >
+                <div className="flex flex-col w-1/3 gap-4 mx-1">
+                  <FormPageOne nextPage={nextPage} />
                 </div>
-                <div className="flex flex-col w-1/2 gap-4">
-                  <h1 className="text-4xl font-bold">Profile Info</h1>
-                  <div className="flex flex-col">
-                    <label htmlFor="first_name">First name</label>
-                    <input
-                      type="text"
-                      name="first_name"
-                      required
-                      className="shadow-black shadow-sm border-transparent border-4 p-1 focus:border-black outline-none rounded-lg"
-                    />
-                  </div>
-                  <div className="flex flex-col">
-                    <label htmlFor="last_name">Last name</label>
-                    <input
-                      type="text"
-                      name="last_name"
-                      required
-                      className="shadow-black shadow-sm border-transparent border-4 p-1 focus:border-black outline-none rounded-lg"
-                    />
-                  </div>
-                  <div className="flex flex-col">
-                    <label htmlFor="username">Username (@ Display name)</label>
-                    <input
-                      type="text"
-                      name="username"
-                      required
-                      className="shadow-black shadow-sm border-transparent border-4 p-1 focus:border-black outline-none rounded-lg"
-                    />
-                  </div>
-                  <div className="flex flex-row justify-center items-center h-16 w-full gap-2">
-                    <button
-                      type="button"
-                      onClick={prevPage}
-                      className="h-full w-full bg-sky-400"
-                    >
-                      Previous
-                    </button>
-                    <button type="submit" className="h-full w-full bg-sky-400">
-                      Sign Up
-                    </button>
-                  </div>
+
+                <div className="flex flex-col w-1/3 gap-4 mx-1">
+                  <FormPageTwo prevPage={prevPage} />
+                </div>
+
+                <div className="flex flex-col w-1/3 gap-4 mx-1">
+                  <FormPageThree />
                 </div>
               </div>
             </form>
