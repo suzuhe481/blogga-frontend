b3abddee54dbd6e8ede544396c77051c30fed190
Hector <baseballhector@gmail.com>
2024-09-15 19:12:44 -0400

    Added animation for quick horizontal shake

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,10 +20,17 @@ export default {
           "50%": { transform: "translateY(10px) rotate(6deg)" },
           "100%": { transform: "translateY(0px) rotate(6deg)" },
         },
+        shake: {
+          "0%": { "margin-left": "0rem" },
+          "25%": { "margin-left": "0.2rem" },
+          "50%": { "margin-left": "-0.2rem" },
+          "100%": { "margin-left": "0rem" },
+        },
       },
       animation: {
         "float-left": "floatLeft 3s ease-in-out infinite",
         "float-right": "floatRight 3s ease-in-out infinite",
+        shake: "shake 0.2s linear 0s 2",
       },
     },
   },
