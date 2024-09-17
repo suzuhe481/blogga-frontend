4661f2dbe8cda840be6b5a4df6e51ca0da72d5fe
Hector <baseballhector@gmail.com>
2024-09-17 18:32:55 -0400

    Changed options on the Editor component

--- src/components/PageComponents/CreateBlogPage/CreateBlogPage.tsx
+++ src/components/PageComponents/CreateBlogPage/CreateBlogPage.tsx
@@ -8,44 +8,45 @@ const CreateBlogPage = () => {
     <div>
       <Navbar />
       <div className="flex justify-center items-center w-full">
-        <div className="w-[95vw] desktop:w-1/2 my-4">
-          <Editor
-            tinymceScriptSrc="/tinymce/tinymce.min.js"
-            licenseKey="gpl"
-            init={{
-              height: 500,
-              menubar: true,
-              resize: true,
-              elementpath: false,
-              promotion: false,
-              plugins: [
-                "advlist",
-                "autolink",
-                "lists",
-                "link",
-                "image",
-                "charmap",
-                "anchor",
-                "searchreplace",
-                "visualblocks",
-                "code",
-                "fullscreen",
-                "insertdatetime",
-                "media",
-                "table",
-                "preview",
-                "help",
-                "wordcount",
-              ],
-              toolbar:
-                "undo redo | blocks | " +
-                "bold italic forecolor | alignleft aligncenter " +
-                "alignright alignjustify | bullist numlist outdent indent | " +
-                "removeformat | help",
-              content_style:
-                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
-            }}
-          />
+            <Editor
+              tinymceScriptSrc="/tinymce/tinymce.min.js"
+              licenseKey="gpl"
+              onEditorChange={(newValue) => handleEditorChange(newValue)}
+              onInit={(_evt, editor) => (editorRef.current = editor)}
+              init={{
+                height: 500,
+                menubar: true,
+                resize: true,
+                elementpath: false,
+                promotion: false,
+                plugins: [
+                  "advlist",
+                  "autolink",
+                  "lists",
+                  "link",
+                  "image",
+                  "charmap",
+                  "anchor",
+                  "searchreplace",
+                  "visualblocks",
+                  "code",
+                  "fullscreen",
+                  "insertdatetime",
+                  "media",
+                  "table",
+                  "preview",
+                  "help",
+                  "wordcount",
+                ],
+                toolbar:
+                  "undo redo | blocks | " +
+                  "bold italic forecolor | alignleft aligncenter " +
+                  "alignright alignjustify | bullist numlist outdent indent | " +
+                  "removeformat | preview | help",
+                content_style:
+                  "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+              }}
+            />
         </div>
       </div>
       <Footer />
