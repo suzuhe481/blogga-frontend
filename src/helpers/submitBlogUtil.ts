2750471f1874a1dbac301f9517a21586978f9154
Hector <baseballhector@gmail.com>
2024-09-17 18:29:08 -0400

    Util function to submit blog post

---
+++ src/helpers/submitBlogUtil.ts
@@ -0,0 +1,35 @@
+const submitBlogUtil = (blogData: { title: string; blog: string }) => {
+  const URL = `${import.meta.env.VITE_DEV_BACKEND_URL}/posts`;
+
+  const headers = {
+    accept: "application/json",
+    "content-type": "application/json",
+  };
+
+  const body = JSON.stringify(blogData);
+
+  return fetch(URL, {
+    method: "POST",
+    headers: headers,
+    credentials: "include",
+    body: body,
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      //   console.log(json);
+      return json;
+    })
+    .catch((error) => {
+      const errorMessage = {
+        error: true,
+        msg: "Failed to connect to server",
+      };
+
+      //   console.log(error);
+      return errorMessage;
+    });
+};
+
+export default submitBlogUtil;
