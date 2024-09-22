e6070fe7ebcd63b6e40d04ebed5590467ac0d061
Hector <baseballhector@gmail.com>
2024-09-22 17:27:45 -0400

    Function to fetch blog data from blog id

---
+++ src/helpers/getBlogUtil.ts
@@ -0,0 +1,32 @@
+const getBlogUtil = (blogId) => {
+  const URL = `${import.meta.env.VITE_DEV_BACKEND_URL}/posts/${blogId}`;
+
+  const headers = {
+    accept: "application/json",
+    "content-type": "application/json",
+  };
+
+  return fetch(URL, {
+    method: "GET",
+    headers: headers,
+    credentials: "include",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      //   console.log(json);
+      return json;
+    })
+    .catch((error) => {
+      const errorMessage = {
+        error: true,
+        msg: "Failed to connect to server",
+      };
+
+      //   console.log(error);
+      return errorMessage;
+    });
+};
+
+export default getBlogUtil;
