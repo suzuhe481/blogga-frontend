a664b6beac0f6d5d97f1bdf0f3dd0a73d543846c
Hector <baseballhector@gmail.com>
2024-09-24 16:48:27 -0400

    topDir variable value depends on dev mode

--- postinstall.js
+++ postinstall.js
@@ -2,7 +2,19 @@
 // https://www.tiny.cloud/docs/tinymce/latest/react-pm-host/
 import fse from "fs-extra";
 import path from "path";
-const topDir = import.meta.dirname;
+
+const devMode = import.meta.VITE_DEV_MODE;
+
+const topDir = devMode
+  ? import.meta.dirname
+  : path.dirname(new URL(import.meta.url).pathname);
+
+// const topDir = import.meta.dirname;
+
+// const topDir = path.dirname(new URL(import.meta.url).pathname);
+
+console.log(topDir);
+
 fse.emptyDirSync(path.join(topDir, "public", "tinymce"));
 fse.copySync(
   path.join(topDir, "node_modules", "tinymce"),
