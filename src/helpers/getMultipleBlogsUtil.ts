d4d26335891aa40f8a56b6134bc2c76c01c4ac70
Hector <baseballhector@gmail.com>
2024-09-28 15:21:28 -0400

    Function to retrieve multiple blog entries

---
+++ src/helpers/getMultipleBlogsUtil.ts
@@ -0,0 +1,34 @@
+const getMultipleBlogsUtil = async (blogCount: number) => {
+  const URL = `${
+    import.meta.env.VITE_DEV_BACKEND_URL
+  }/posts?blogCount=${blogCount}`;
+
+  const headers = {
+    accept: "application/json",
+    "content-type": "application/json",
+  };
+
+  return fetch(URL, {
+    method: "GET",
+    headers: headers,
+    credentials: "include",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      //   console.log(json);
+      return json;
+    })
+    .catch((error) => {
+      const errorMessage = {
+        error: true,
+        msg: "Failed to connect to server",
+      };
+
+      //   console.log(error);
+      return errorMessage;
+    });
+};
+
+export default getMultipleBlogsUtil;
