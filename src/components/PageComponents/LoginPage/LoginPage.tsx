f8f45208931f27f2f7bebaceb1a3acb21305bbad
Hector <baseballhector@gmail.com>
2024-10-12 19:09:10 -0400

    Initial login page component

---
+++ src/components/PageComponents/LoginPage/LoginPage.tsx
@@ -0,0 +1,15 @@
+import SignInForm from "./SignInForm";
+import RegisterForm from "./RegisterForm";
+import Footer from "../Welcome/Footer";
+
+const LoginPage = () => {
+  return (
+    <div className="relative flex flex-col justify-between min-h-screen">
+      {/* <SignInForm /> */}
+      {/* <RegisterForm /> */}
+      <Footer />
+    </div>
+  );
+};
+
+export default LoginPage;
