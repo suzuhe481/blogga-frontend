c4d50483fc7a77415f28089aae4c1596b8182ff6
Hector <baseballhector@gmail.com>
2024-10-14 18:21:15 -0400

    Changed href to login and register forms

--- src/components/UI/Navbar/MenuPublic/MenuPublic.tsx
+++ src/components/UI/Navbar/MenuPublic/MenuPublic.tsx
@@ -2,13 +2,13 @@ const MenuPublic = () => {
   return (
     <div className="flex flex-row text-nowrap justify-center items-center gap-1 desktop:gap-6 h-fullk">
       <a
-        href="/login"
+        href="/login/#login"
         className="font-bold hover:text-sky-400 text-sm desktop:text-lg text-nowrap p-2 flex flex-row justify-center items-center"
       >
         Log in
       </a>
       <a
-        href="/signup"
+        href="/login/#register"
         className="text-sky-400 font-bold text-sm text-nowrap desktop:text-lg p-2 border-2 border-sky-400 rounded-md cursor-pointer
     
     hover:text-white 
