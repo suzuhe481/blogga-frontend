6f53f311fba8ac1c9ea1ee977f02d2d7d1102c38
Hector <baseballhector@gmail.com>
2024-11-06 21:10:55 -0500

    Author's name is a link to view their other blogs

--- src/components/PageComponents/BrowseBlogsPage/BlogCard.tsx
+++ src/components/PageComponents/BrowseBlogsPage/BlogCard.tsx
@@ -20,8 +20,6 @@ const BlogCard = ({ title, author, date, shortId, authorID }: IBlogCard) => {
     return textarea.value;
   }
 
-  const author_name = `${author.first_name} ${author.last_name}`;
-
   return (
     <a
       href={`/blog/${shortId}`}
@@ -42,9 +40,10 @@ const BlogCard = ({ title, author, date, shortId, authorID }: IBlogCard) => {
             className="mb-2 group-hover:underline font-bold"
           />
           <div className="flex flex-row gap-4 mb-2">
-            <div
+            <a
+              href={`/user/${authorID}/blogs`}
               dangerouslySetInnerHTML={{
-                __html: decodeHtml(DOMPurify.sanitize(author_name)),
+                __html: decodeHtml(DOMPurify.sanitize(author)),
               }}
             />
             <div>|</div>
