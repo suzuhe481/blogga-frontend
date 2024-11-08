7f4f863199849bacf1307852681c086963d90175
Hector <baseballhector@gmail.com>
2024-11-08 18:10:48 -0500

    Redesigned error page

--- src/components/PageComponents/ErrorPage/ErrorMessage.tsx
+++ src/components/PageComponents/ErrorPage/ErrorMessage.tsx
@@ -1,33 +1,18 @@
-import { useState } from "react";
-
-import LOGO_BLACK from "../../../assets/images/LOGO_BLACK.png";
-import LOGO_WHITE from "../../../assets/images/LOGO_WHITE.png";
+import LOGO_BLUE from "../../../assets/images/LOGO_BLUE.png";
 
 const ErrorMessage = () => {
-  const [logo, setLogo] = useState(LOGO_WHITE);
-
-  // Changes logo to BLUE when moure enters.
-  const mouseEnterHandler = () => {
-    setLogo(LOGO_BLACK);
-  };
-
-  // Changes logo to default BLACK when mouse leaves.
-  const mouseLeaveHandler = () => {
-    setLogo(LOGO_WHITE);
-  };
-
   return (
-    <div className="flex flex-col shadow-xl justify-center gap-2 items-center h-screen bg-sky-400 text-2xl desktop:text-5xl text-white font-FuzzyBubbles font-bold desktop:gap-0">
-      <p>Sorry, this page doesn't exist.</p>
-      <p>:(</p>
-      <a href="/">
-        <img
-          src={logo}
-          onMouseEnter={mouseEnterHandler}
-          onMouseLeave={mouseLeaveHandler}
-          className="h-20 object-contain aspect-auto desktop:h-60"
-        />
-      </a>
+    <div className="flex flex-col items-center">
+      <div className="flex flex-col text-center shadow-2xl w-[90vw] desktop:w-[50vw] rounded-xl justify-center gap-2 items-center text-sky-400 text-2xl desktop:text-5xl font-FuzzyBubbles font-bold desktop:gap-0">
+        <p>Sorry, this page doesn't exist.</p>
+        <p>:(</p>
+        <a href="/">
+          <img
+            src={LOGO_BLUE}
+            className="h-20 object-contain aspect-auto desktop:h-60"
+          />
+        </a>
+      </div>
     </div>
   );
 };
