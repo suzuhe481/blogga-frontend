3284194d3a34430f37624249043ac450472b1d4c
Hector <baseballhector@gmail.com>
2024-11-08 20:06:52 -0500

    Removed unused components

--- src/components/PageComponents/Profile/Profile.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Profile.module.scss";
-
-const Profile = () => {
-  return <div className={styles.profile}>This is a profile page</div>;
-};
-
-export default Profile;
--- src/components/PageComponents/Search/Search.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./Search.module.scss";
-
-const Search = () => {
-  return <div className={styles.search}>This is the search page</div>;
-};
-
-export default Search;
