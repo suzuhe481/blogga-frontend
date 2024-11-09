5b4be8a15668cf4b9d52cb68e00ba783572face7
Hector <baseballhector@gmail.com>
2024-11-09 17:49:55 -0500

    Style changes. Added loading spinner and error message

--- src/components/PageComponents/Blog/BlogView.tsx
+++ src/components/PageComponents/Blog/BlogView.tsx
@@ -6,42 +6,47 @@ import { DateTime } from "luxon";
 
 import getBlogUtil from "../../../helpers/getBlogUtil";
 
+import { ring } from "ldrs";
+
 export interface Iblog {
   title: string;
   date: string;
-  blog: string;
+  post: string;
   author: string;
   shortId: string;
+  authorID: string;
 }
 
 const BlogView = () => {
-  // false = Blog could not be loaded.
-  // null = blog is being fetched
-  const [blog, setBlog] = useState<Iblog | false | null>(null);
+  ring.register();
+
+  // null - No blog is loaded
+  const [blog, setBlog] = useState<Iblog | null>(null);
   const { id = "" } = useParams() as { id: string }; // Fallback value of ""
+  const [blogLoading, setBlogLoading] = useState<boolean>(true);
+  const [blogError, setBlogError] = useState<boolean>(false);
 
-  // Loading and error elements for blog.
-  const BlogLoading = <div>Blog loading...</div>;
-  const BlogError = <div>Blog could not load</div>;
-
-  // The final blog view.
-  // The blog state, determines the view show..
-  const BlogView = blog ? (
-    <>
-      <h1 className="w-full">{blog.title}</h1>
-      <p>{DateTime.fromISO(blog.date).toLocaleString(DateTime.DATETIME_MED)}</p>
-      <p>{blog.date}</p>
-      <div
-        className="w-full"
-        dangerouslySetInnerHTML={{
-          __html: decodeHtml(DOMPurify.sanitize(blog.blog)),
-        }}
+  // Display loading spinner.
+  // Loading animation is fixed in the center of the screen.
+  const loadingAnimation = (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+      <l-ring
+        size="100"
+        stroke="15"
+        bg-opacity="0.4"
+        speed="2"
+        color="rgb(59, 189, 248)"
       />
-    </>
-  ) : !blog ? (
-    BlogError
-  ) : (
-    BlogLoading
+    </div>
+  );
+
+  // Error element.
+  const blogErrorMessage = (
+    <div className="flex flex-col items-center">
+      <div className="flex flex-col text-center shadow-2xl w-[90vw] desktop:w-[50vw] rounded-xl justify-center gap-2 items-center text-sky-400 text-2xl desktop:text-5xl font-FuzzyBubbles font-bold desktop:gap-0">
+        <p>Blog doesn't exist or there was an error.</p>
+      </div>
+    </div>
   );
 
   // Function to decode HTML string of blog from database.
@@ -56,25 +61,48 @@ const BlogView = () => {
   useEffect(() => {
     getBlogUtil(id).then((result) => {
       if (result.error === true) {
-        setBlog(false);
+        setBlogError(true);
+        setBlogLoading(false);
         return;
       }
 
-      const blog: Iblog = {
-        title: result.post.title,
-        date: result.post.date,
-        blog: result.post.post,
-        author: result.post.author,
-        shortId: result.post.shortId,
-      };
-
+      const blog: Iblog = result.post;
       setBlog(blog);
+      setBlogLoading(false);
     });
   }, []);
 
   return (
     <div className="flex flex-col items-center justify-start flex-grow mx-4 desktop:mx-48 py-4">
-      {BlogView}
+      {blogLoading ? loadingAnimation : null}
+      {blogError ? blogErrorMessage : null}
+      {blog ? (
+        <div className="flex flex-col w-full gap-4">
+          <div className="flex flex-col gap-1">
+            <h1 className="w-full font-bold text-2xl desktop:text-4xl">
+              {blog.title}
+            </h1>
+            <div>
+              <a
+                href={`/user/${blog.authorID}/blogs`}
+                className="font-bold text-sky-600 hover:underline"
+              >
+                {blog.author}
+              </a>
+              <p>
+                {DateTime.fromISO(blog.date).toLocaleString(DateTime.DATE_FULL)}
+              </p>
+            </div>
+          </div>
+          <hr />
+          <div
+            className="w-full"
+            dangerouslySetInnerHTML={{
+              __html: decodeHtml(DOMPurify.sanitize(blog.post)),
+            }}
+          />
+        </div>
+      ) : null}
     </div>
   );
 };
