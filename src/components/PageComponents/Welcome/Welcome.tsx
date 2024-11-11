88a309020078b5b3a2a17b15ed72bcb5b30bdebc
Hector <baseballhector@gmail.com>
2024-11-10 20:48:35 -0500

    Moved FeaturesData. Added a gap between the page components

--- src/components/PageComponents/Welcome/Welcome.tsx
+++ src/components/PageComponents/Welcome/Welcome.tsx
@@ -1,24 +1,16 @@
 import Navbar from "../../UI/Navbar/Navbar";
-import LOGO_WHITE from "../../../assets/images/LOGO_WHITE.png";
 
 import Header from "./Header";
 import FeatureCards from "./FeatureCards";
 import Footer from "../../UI/Footer/Footer";
 
 const Welcome = () => {
-  const FeaturesData = [
-    "A new blogging website!",
-    "Share your stories, ideas, articles, and more!",
-    "Rich text editor to emphasize your words, your way.",
-    "Stay tuned. Blogga is coming soon!",
-  ];
-
   return (
     <div className="overflow-hidden min-h-screen">
       <Navbar />
-      <div className="flex flex-col justify-between min-h-screen">
+      <div className="flex flex-col justify-between min-h-screen gap-4">
         <Header />
-        <FeatureCards featuresData={FeaturesData} />
+        <FeatureCards />
         <Footer />
       </div>
     </div>
